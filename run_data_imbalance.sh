439a5b878b45b01e103dee45f70a61c0a34b9931
UlkuTuncerKucuktas <ulku.kucuktas@radius.ai>
2023-12-16 22:24:25 +0000

    scripts

--- run_data_imbalance.sh
+++ run_data_imbalance.sh
@@ -1,1 +1,1 @@
-python3 create_dataset_data_imbalance.py --image_folder /content/fed_dentex/dentex_multi/images --label_folder /content/fed_dentex/dentex_multi/labels --dest_base_path /content/fed_dentex/dentex_split --train_split 0.9 --additional_splits 0.15 0.20 0.25 0.40
+python3 create_dataset_data_imbalance.py --image_folder ./dentex_multi/images --label_folder ./dentex_multi/labels --dest_base_path ./dentex_split --train_split 0.9 --additional_splits 0.15 0.20 0.25 0.40
