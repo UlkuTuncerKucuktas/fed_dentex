9ccd7ad99ef3a3335ffb938b884576b0cc224c0c
UlkuTuncerKucuktas <ulku.kucuktas@radius.ai>
2023-12-23 22:02:18 +0000

    federated splitted

--- train_federated.py
+++ train_federated.py
@@ -19,7 +19,6 @@ def parse_arguments():
     parser.add_argument('--weighted_avg', action='store_true', help='Use weighted averaging based on validation performance')
     return parser.parse_args()
 
-
 def delete_weights_folders(directory):
     for root, dirs, files in os.walk(directory, topdown=False):
         for name in dirs:
@@ -37,7 +36,7 @@ def average_models(models):
         averaged_params[param_name] = torch.mean(params, dim=0)
     return averaged_params
 
-def plot_metrics(metrics, title='Training Metrics'):
+def plot_metrics(metrics, title, filename):
     plt.figure(figsize=(10, 5))
     for metric in metrics:
         plt.plot(metrics[metric], label=metric)
@@ -46,17 +45,15 @@ def plot_metrics(metrics, title='Training Metrics'):
     plt.title(title)
     plt.legend()
     plt.grid(True)
-    plt.savefig(f'/content/drive/MyDrive/federated_results/{title}.png')
+    plt.savefig(filename)
+    plt.close()
 
-def evaluate_model(model_state, data_config,config):
+def evaluate_model(model_state, data_config, config):
     model = YOLO(config)
     model.model.load_state_dict(model_state)
     results = model.val(data=data_config)
     mean_precision, mean_recall, mean_map50, mean_map_50_95 = results.mean_results()
-    # Using the mean_results method to get mean precision, recall, mAP50, and mAP50-95
-    mean_precision, mean_recall, mean_map50, mean_map_50_95 = results.mean_results()
     return {'precision': mean_precision, 'recall': mean_recall, 'map50': mean_map50, 'map_50_95': mean_map_50_95}
-
 
 def main():
     args = parse_arguments()
@@ -64,7 +61,7 @@ def main():
     # Disable WandB
     os.environ['WANDB_DISABLED'] = 'true'
     temp_dir = '/content/fed_dentex/runs'
-    metrics = {'map50': [], 'precision': [], 'recall': [],'map_50_95': []}
+    split_metrics = {split: {'map50': [], 'precision': [], 'recall': [], 'map_50_95': []} for split in args.train_splits}
 
     model = YOLO(args.config).load(args.model)
 
@@ -77,39 +74,36 @@ def main():
 
         models = [copy.deepcopy(model) for _ in range(len(args.train_splits))]
 
-        validation_scores = []
-
         for index, dup_model in enumerate(models):
-            dup_model.train(data=args.train_splits[index], epochs=args.local_epochs, batch=args.batch,save=True, resume=True, workers=0)
+            dup_model.train(data=args.train_splits[index], epochs=args.local_epochs, batch=args.batch, save=True, resume=True, workers=0)
             dup_model.zero_grad()
 
             if os.path.exists(temp_dir):
                 delete_weights_folders(temp_dir)
 
-
         ensemble_model_params = average_models(models)
-
         model.model.load_state_dict(ensemble_model_params)
 
-        # Evaluate the averaged model
-        print("Evaluate " , comm_round , "ROUND" , "------------------------------------------")
-        eval_results = evaluate_model(ensemble_model_params, args.train_splits[0],args.config)
-        metrics['map50'].append(eval_results['map50'])
-        metrics['map_50_95'].append(eval_results['map_50_95'])
-        metrics['precision'].append(eval_results['precision'])
-        metrics['recall'].append(eval_results['recall'])
+        # Evaluate the averaged model on all splits
+        for split in args.train_splits:
+            eval_results = evaluate_model(ensemble_model_params, split, args.config)
+            for key in split_metrics[split]:
+                split_metrics[split][key].append(eval_results[key])
 
-        if eval_results['map50'] > best_map:
-          best_map = eval_results['map50']
-          torch.save(model, '/content/drive/MyDrive/federated_results/best.pt')
-
-        
+            if eval_results['map50'] > best_map:
+                best_map = eval_results['map50']
+                torch.save(model, '/content/drive/MyDrive/federated_results/best.pt')
 
         gc.collect()
         if torch.cuda.is_available():
             torch.cuda.empty_cache()
 
-    plot_metrics(metrics, title='Federated Training Metrics')
+    # Plot and save metrics for each split
+    for split in args.train_splits:
+        split_name = os.path.basename(split).split('.')[0]
+        plot_metrics(split_metrics[split], title=f'Federated Training Metrics - {split_name}', 
+                     filename=f'/content/drive/MyDrive/federated_results/metrics_{split_name}.png')
+
     torch.save(model, '/content/drive/MyDrive/federated_results/final_federated_model.pt')
 
 if __name__ == "__main__":
